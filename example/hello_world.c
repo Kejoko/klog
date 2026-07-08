98b55777bb0cda783855caed831cf929b437e7b0
Keegan Kochis <keeganjkochis@gmail.com>
2026-07-07 22:18:44 -0600

    Example showcasing how the newline splitting works with the max character length

--- example/hello_world.c
+++ example/hello_world.c
@@ -8,6 +8,7 @@
 int main(
     void
 ) {
+    /* We are using relatively short lines (10 chars) to easily display newline behavior */
     KlogFormatInfo  format_info  = { 10, 100, 0, false, false };
     KlogConsoleInfo console_info = { KLOG_LEVEL_TRACE, true };
     klog_initialize(3, format_info, NULL, &console_info, NULL, NULL);
@@ -15,6 +16,7 @@ int main(
     const KlogLoggerHandle* p_handle = klog_logger_create("my_logger", 9);
     klog_logger_level_set(p_handle, KLOG_LEVEL_TRACE);
     klog_info(p_handle, "Hello world!");
+    klog_info(p_handle, "We can use printf formatting, look! %s %#x %f %c%c", "42", 66, 42.42f, 52, '2');
 
     klog_deinitialize();
 
