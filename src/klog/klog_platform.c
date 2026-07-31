372e051dfddba8021c80d56f169fa7a18df7d95c
Keegan Kochis <keeganjkochis@gmail.com>
2026-07-31 15:51:50 -0600

    Fixed width integers in platform return codes

--- src/klog/klog_platform.c
+++ src/klog/klog_platform.c
@@ -17,7 +17,7 @@ procid_t klog_platform_get_current_thread_id(
 void klog_platform_mutex_initialize(
     klog_platform_mutex_t* p_mutex
 ) {
-    int result = pthread_mutex_init(p_mutex, NULL); /* "pthread_mutex_init() always returns 0" - from the man page ????? */
+    int32_t result = pthread_mutex_init(p_mutex, NULL); /* "pthread_mutex_init() always returns 0" - from the man page ????? */
     if (result == EBUSY) {
         kdprintf("pthread_mutex_init returned %d: Mutex is already initialized\n", result);
         exit(KLOG_EXIT_CODE);
@@ -46,7 +46,7 @@ void klog_platform_mutex_initialize(
 void klog_platform_mutex_deinitialize(
     klog_platform_mutex_t* p_mutex
 ) {
-    int result = pthread_mutex_destroy(p_mutex);
+    int32_t result = pthread_mutex_destroy(p_mutex);
     if (result == EBUSY) {
         kdprintf("pthread_mutex_destroy returned %d: Attempt to destroy mutex while it is locked\n", result);
         exit(KLOG_EXIT_CODE);
@@ -60,7 +60,7 @@ void klog_platform_mutex_deinitialize(
 void klog_platform_mutex_lock(
     klog_platform_mutex_t* p_mutex
 ) {
-    int result = pthread_mutex_lock(p_mutex);
+    int32_t result = pthread_mutex_lock(p_mutex);
     if (result == EINVAL) {
         kdprintf("pthread_mutex_lock returned %d: Mutex is not initialized\n", result);
         exit(KLOG_EXIT_CODE);
@@ -86,7 +86,7 @@ void klog_platform_mutex_lock(
 void klog_platform_mutex_unlock(
     klog_platform_mutex_t* p_mutex
 ) {
-    int result = pthread_mutex_unlock(p_mutex);
+    int32_t result = pthread_mutex_unlock(p_mutex);
     if (result == EINVAL) {
         kdprintf("pthread_mutex_unlock returned %d: Mutex is not initialized\n", result);
         exit(KLOG_EXIT_CODE);
@@ -113,7 +113,7 @@ void klog_platform_semaphore_initialize(
     klog_platform_semaphore_t* p_semaphore,
     uint32_t                   count
 ) {
-    int retval = sem_init(p_semaphore, 0, count);
+    int32_t retval = sem_init(p_semaphore, 0, count);
     if (retval != 0) {
         kdprintf("Error in klog_platform_semaphore_initialize: sem_init returned %d - errno: %s\n", retval, strerror(errno));
         exit(KLOG_EXIT_CODE);
@@ -123,7 +123,7 @@ void klog_platform_semaphore_initialize(
 void klog_platform_semaphore_deinitialize(
     klog_platform_semaphore_t* p_semaphore
 ) {
-    const int retval = sem_destroy(p_semaphore);
+    const int32_t retval = sem_destroy(p_semaphore);
     if (retval != 0) {
         kdprintf("Error in klog_platform_semaphore_deinitialize: sem_destroy returned %d - errno: %s\n", retval, strerror(errno));
         exit(KLOG_EXIT_CODE);
@@ -133,7 +133,7 @@ void klog_platform_semaphore_deinitialize(
 void klog_platform_semaphore_wait(
     klog_platform_semaphore_t* p_semaphore
 ) {
-    const int retval = sem_wait(p_semaphore);
+    const int32_t retval = sem_wait(p_semaphore);
     if (retval != 0) {
         kdprintf("Error in klog_platform_semaphore_wait: sem_wait returned %d - errno: %s\n", retval, strerror(errno));
         exit(KLOG_EXIT_CODE);
@@ -143,18 +143,18 @@ void klog_platform_semaphore_wait(
 void klog_platform_semaphore_signal(
     klog_platform_semaphore_t* p_semaphore
 ) {
-    const int retval = sem_post(p_semaphore);
+    const int32_t retval = sem_post(p_semaphore);
     if (retval != 0) {
         kdprintf("Error in klog_platform_semaphore_signal: sem_post returned %d - errno: %s\n", retval, strerror(errno));
         exit(KLOG_EXIT_CODE);
     }
 }
 
-int klog_platform_semaphore_value_get(
+int32_t klog_platform_semaphore_value_get(
     klog_platform_semaphore_t* p_semaphore
 ) {
-    int       result = 0;
-    const int retval = sem_getvalue(p_semaphore, &result);
+    int32_t       result = 0;
+    const int32_t retval = sem_getvalue(p_semaphore, &result);
     if (retval != 0) {
         kdprintf("Error in klog_platform_semaphore_value_get: sem_getvalue returned %d - errno: %s\n", retval, strerror(errno));
         exit(KLOG_EXIT_CODE);
@@ -169,7 +169,7 @@ void klog_platform_thread_create(
     ),
     void*                   p_arg
 ) {
-    int result = pthread_create(p_thread, NULL, thread_body, p_arg);
+    int32_t result = pthread_create(p_thread, NULL, thread_body, p_arg);
     if (result == EAGAIN) {
         kdprintf("pthread_create returned %d: Insufficient resources to create another thread\n", result);
         exit(KLOG_EXIT_CODE);
@@ -191,7 +191,7 @@ void klog_platform_thread_join(
     klog_platform_thread_t* p_thread,
     void**                  p_ret
 ) {
-    int result = pthread_join(*p_thread, p_ret);
+    int32_t result = pthread_join(*p_thread, p_ret);
     if (result == EDEADLK) {
         kdprintf("pthread_join returned %d: A deadlock was detected, or we are trying to join the calling thread (ourself)\n", result);
         exit(KLOG_EXIT_CODE);
