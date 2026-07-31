85a76dfc7aabc04eaa993a3a10b93c31dcea2be4
Keegan Kochis <keeganjkochis@gmail.com>
2026-07-31 16:00:33 -0600

    Increment version 0.0.18

--- cmake/KlogVersion.cmake
+++ cmake/KlogVersion.cmake
@@ -21,4 +21,5 @@ endfunction()
 # set_klog_versions(0 0 14) # Pre-allocate multiple formatted input message buffer to prep for multi-threading support
 # set_klog_versions(0 0 15) # Don't allocate when formatting logger names
 # set_klog_versions(0 0 16) # Examples!
-set_klog_versions(0 0 17) # Async logging with dedicated threads
+# set_klog_versions(0 0 17) # Async logging with dedicated threads
+set_klog_versions(0 0 18) # Async logging improvements
