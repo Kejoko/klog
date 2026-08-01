47ea150a0b9925cffca85f03e46e0aab23ceb4fc
Keegan Kochis <keeganjkochis@gmail.com>
2026-08-01 15:32:55 -0600

    Update test script to show what command we are currently executing so we can investigate hangs

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -16,6 +16,21 @@ BGYELLOW=$(tput setab 3)
 BGMAGENTA=$(tput setab 5)
 NORMAL=$(tput sgr0) # Resets all attributes
 
+# Error / success messages
+# These look bad when declared here but they are all aligned for nicely printing
+P_EF="${FGRED}PASSED      (should fail)${NORMAL}"
+ME_EF="${FGYELLOW}MEMERR      (should fail)${NORMAL}"
+F_EF="${NORMAL}failed      (should fail)${NORMAL}"
+P_EP="${NORMAL}passed      (should pass)${NORMAL}"
+ME_EP="${FGYELLOW}MEMERR      (should pass)${NORMAL}"
+F_EP="${FGRED}FAILED      (should pass)${NORMAL}"
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Where we actually store the results (initialize an empty list)                                                       #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+failed_commands=()
+
 # -------------------------------------------------------------------------------------------------------------------- #
 # Determine which tests should fail, and for which tests we should ignore the valgrind information                     #
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -35,50 +50,39 @@ if [ -f "$ignore_memory_list_file" ]; then
 fi
 
 # -------------------------------------------------------------------------------------------------------------------- #
-# Determine which tests to run, and run them all, making note of which ones failed and in what fashion                 #
+# Helper function to actually run the test and check the error result                                                  #
 # -------------------------------------------------------------------------------------------------------------------- #
 
-# Create list of all tests from the ft and ut directories
-uts_to_run=(./bin/ut/*)
-fts_to_run=(./bin/ft/*)
-all_tests_to_run=("${uts_to_run[@]}" "${fts_to_run[@]}")
+run_test_update_failed_commands () {
+    local arg_test_command="$1"
+    local arg_command_to_run="$2"
+    local arg_is_valgrind="$3"
 
-# These look bad when declared here but they are all aligned for nicely printing
-P_EF="${FGRED}PASSED (should fail)${NORMAL}"
-ME_EF="${FGYELLOW}MEMERR (should fail)${NORMAL}"
-F_EF="failed (should fail)${NORMAL}"
-P_EP="passed (should pass)${NORMAL}"
-ME_EP="${FGYELLOW}MEMERR (should pass)${NORMAL}"
-F_EP="${FGRED}FAILED (should pass)${NORMAL}"
+    local arg_test_name=$(basename "$arg_test_command")
 
-exit_code=0
-
-# Do the thing (execute all tests, and take note of failed commands)
-failed_commands=()
-for test_command in "${all_tests_to_run[@]}"; do
-    test_name=$(basename "$test_command")
-
-    if [[ "$test_name" == "*" ]]; then
-        continue
-    fi
-
-    command_to_run="$test_command"
-
-    if [[ ! " ${ignore_memory_list[*]} " =~ " ${test_name} " ]]; then
-        command_to_run="valgrind --tool=memcheck --enable-debuginfod=no --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all --error-exitcode=54 $command_to_run"
+    # Align this output with our error/pass result outputs
+    if [ "$arg_is_valgrind" -eq 0 ]; then
+        #    "passed      (should pass) : "
+        echo "executing                 : $arg_test_name"
+    else
+        #    "passed      (should pass) : "
+        echo "valgrinding               : $arg_test_name"
     fi
 
     $command_to_run > /dev/null 2>&1
-    status=$?
+    local status=$?
 
-    result_message="PASSED"
-    if [[ " ${expected_failure_list[*]} " =~ " ${test_name} " ]]; then # If we should have failed
+    local did_expected=1
+    local result_message="PLACEHOLDER_RESULT"
+    if [[ " ${expected_failure_list[*]} " =~ " ${arg_test_name} " ]]; then # If we should have failed
         if [ "$status" -eq 0 ]; then
             result_message=$P_EF
             failed_commands+=( "$command_to_run" )
+            did_expected=0
         elif [ "$status" -eq 54 ]; then
             result_message=$ME_EF
             failed_commands+=( "$command_to_run" )
+            did_expected=0
         else
             result_message=$F_EF
         fi
@@ -88,13 +92,63 @@ for test_command in "${all_tests_to_run[@]}"; do
         elif [ "$status" -eq 54 ]; then
             result_message=$ME_EP
             failed_commands+=( "$command_to_run" )
+            did_expected=0
         else
             result_message=$F_EP
             failed_commands+=( "$command_to_run" )
+            did_expected=0
         fi
     fi
+    
+    echo "$result_message : $arg_test_name"
 
-    echo "$result_message : $test_name"
+    if [ "$did_expected" -eq 1 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Determine which tests to run, and run them all, making note of which ones failed and in what fashion                 #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Create list of all tests from the ft and ut directories
+uts_to_run=(./bin/ut/*)
+fts_to_run=(./bin/ft/*)
+all_tests_to_run=("${uts_to_run[@]}" "${fts_to_run[@]}")
+
+# Do the thing (execute all tests, and take note of failed commands)
+for test_command in "${all_tests_to_run[@]}"; do
+    test_name=$(basename "$test_command")
+
+    if [[ "$test_name" == "*" ]]; then
+        continue
+    fi
+
+    # ------------------------------------------------------------------------------------------------------ #
+    # First pass: run the test without valgrind to ensure the logic is valid                                 #
+    # ------------------------------------------------------------------------------------------------------ #
+    
+    command_to_run="$test_command"
+
+    run_test_update_failed_commands "$test_command" "$command_to_run" 0
+
+    passed=$?
+
+    if [ $passed -eq 1 ]; then
+        continue
+    fi
+
+    # ------------------------------------------------------------------------------------------------------ #
+    # Second pass: run the test with valgrind to ensure the memory usage is valid                            #
+    # ------------------------------------------------------------------------------------------------------ #
+
+    if [[ ! " ${ignore_memory_list[*]} " =~ " ${test_name} " ]]; then
+        command_to_run="valgrind --tool=memcheck --enable-debuginfod=no --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all --error-exitcode=54 $command_to_run"
+
+        run_test_update_failed_commands "$test_command" "$command_to_run" 1
+    fi
 done
 
 echo ""
